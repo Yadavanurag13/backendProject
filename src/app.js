644a09fde6d32029df06cacbad0f4cb68ae8720d
Anurag Yadav <yadavanurag1310@gmail.com>
2024-04-11 12:23:20 +0530

    changes commit

--- src/app.js
+++ src/app.js
@@ -17,13 +17,26 @@ app.use(cors({
 //when data is comming from "form" we use this method to get the data
 app.use(express.json({limit: "16kb"}))
 
-app.use(express.urlencoded()) // this will also work
+app.use(express.urlencoded({extended: true, limit: "16kb"})) // this will also work
 //app.use(express.urlencoded({extended: true, limit: "16kb"}))
 
-//public folder to make 
-app.use(express.static)
+ 
+app.use(express.static("public"))
 
 //cookieParser
 app.use(cookieParser)
+
+
+
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+//routes decleartion
+app.use("/api/v1/users", userRouter)
+
+//url that will be formed
+//http://localost:8000/api/v1/users/
+
 
 export {app}
